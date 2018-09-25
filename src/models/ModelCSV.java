93da8c0e03820fce27a6c72a1705dcac5fd9682e
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-09-25 12:51:55 -0500

    Se realizo las variables que seran necesarias para el proceso Se crearon los Getter y Setter de las variables

--- src/models/ModelCSV.java
+++ src/models/ModelCSV.java
@@ -10,5 +10,31 @@ package models;
  * @author manl_
  */
 public class ModelCSV {
-    
+    private String nombre;
+    private String email;
+    private String path = "C:\\archivos\\datos.csv";
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 }
