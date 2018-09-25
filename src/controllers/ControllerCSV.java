1ec866be37d506a13c80eb47ff35fb820558dadc
MarioAlbertoNL1799 <37378931+MarioAlbertoNL1799@users.noreply.github.com>
2018-09-25 17:49:42 -0500

    0.- Se realizo la estructura que indica que acciones se llevaran a cabo en cada boton y sus procesos relacionados con el modelo y la vista. 1.- Se activa la vista para poder ejecutar el programa

--- src/controllers/ControllerCSV.java
+++ src/controllers/ControllerCSV.java
@@ -4,11 +4,50 @@
  * and open the template in the editor.
  */
 package controllers;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import views.ViewCSV;
+import models.ModelCSV;
 
 /**
  *
  * @author manl_
  */
 public class ControllerCSV {
+    ModelCSV modelCSV;
+    ViewCSV viewCSV;
     
+    ActionListener al = new ActionListener(){
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(e.getSource() == viewCSV.jb_niuevo)
+                jb_nuevo_actionPerformed();
+            else if(e.getSource() == viewCSV.jb_guardar)
+                jb_guardar_actionPerformed();
+        }
+    };   
+    public void jb_nuevo_actionPerformed(){
+        modelCSV.readFile();
+        viewCSV.jtf_nombre.setText(null);
+        modelCSV.setNombre(null);
+        viewCSV.jtf_email.setText(null);
+        modelCSV.setEmail(null);
+    }
+
+    public ControllerCSV(ModelCSV modelCSV, ViewCSV viewCSV) {
+        this.modelCSV = modelCSV;
+        this.viewCSV = viewCSV;
+        this.viewCSV.jb_niuevo.addActionListener(al);
+        this.viewCSV.jb_guardar.addActionListener(al);
+        initComponents();
+    }
+    public void jb_guardar_actionPerformed(){
+       modelCSV.readFile();
+       modelCSV.setNombre(viewCSV.jtf_nombre.getText());
+       modelCSV.setEmail(viewCSV.jtf_email.getText());
+       modelCSV.writeFile();
+    }
+    public void initComponents(){
+       this.viewCSV.setVisible(true);
+    }
 }
